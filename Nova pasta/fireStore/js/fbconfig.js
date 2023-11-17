61b90810e0ba4d3b8ac195f002090332a9777916
ThalibNuredin <141377097+ThalibNuredin@users.noreply.github.com>
2023-11-16 21:30:12 -0300

    Add files via upload

---
+++ Nova pasta/fireStore/js/fbconfig.js
@@ -0,0 +1,19 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyB0CLDzjszZgzppgnMVbpiQ-AORyhhsci0",
+    authDomain: "test123-326b8.firebaseapp.com",
+    databaseURL: "https://test123-326b8-default-rtdb.firebaseio.com",
+    projectId: "test123-326b8",
+    storageBucket: "test123-326b8.appspot.com",
+    messagingSenderId: "268644934542",
+    appId: "1:268644934542:web:9996af49a8c45a19bb7adf",
+    measurementId: "G-7XPMEPLHPY"
+  };
+    // Initialize Firebase
+const app = firebase.initializeApp(firebaseConfig);
+  
+    // Firebase Database Reference and the child
+//const dbRef = firebase.database().ref();
+//const pessoaRef = dbRef.child('avaliacaoip');
+
+var storage = firebase.storage();
+  
